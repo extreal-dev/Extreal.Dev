a619105d3afa294b00918e936de0d583d430886e
Cho Kinken <37402719+mahaweilu@users.noreply.github.com>
2024-04-09 14:43:17 +0900

    release-1.3.0 (#6)
    Co-authored-by: sus-taguchi-t <taguchi-t@m.sus-g.co.jp>

--- WebGLBuild/index.ts
+++ WebGLBuild/index.ts
@@ -1,3 +1,14 @@
-import { serve } from "https://deno.land/std@0.192.0/http/mod.ts";
 import { serveDir } from "https://deno.land/std@0.192.0/http/file_server.ts";
-serve((req) => serveDir(req, { fsRoot: "./Dev/" }), { port: 3333 });
+Deno.serve(
+    { port: 3333 },
+    (req) => {
+        const path = new URL(req.url).pathname;
+        if (path.startsWith("/assets")) {
+            return serveDir(req, { fsRoot: "./Data/" });
+        }
+        if (path.startsWith("/Panorama")) {
+            return serveDir(req, { fsRoot: "./PanoramicData/" });
+        }
+        return serveDir(req, { fsRoot: "./Dev/" });
+    },
+);
